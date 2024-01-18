e883a9688d81b737527087a44e23cd21dcd87030
Shiradil <shirbaev04@mail.ru>
2024-01-19 02:04:59 +0600

    First commit

--- server/cmd/cmd.go
+++ server/cmd/cmd.go
@@ -9,10 +9,11 @@ import (
 
 func RunServer() {
 	http.Handle("/client/templates/", http.StripPrefix("/client/templates/", http.FileServer(http.Dir("./client/templates"))))
-	port := ":8080"
+
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/generate", handlers.GenerateHandler)
-
+	
+	port := ":8080"
 	fmt.Printf("Starting server on localhost%s...\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
